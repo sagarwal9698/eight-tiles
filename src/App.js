090e4fe30bb49cb93257b070558813763381eaf0
Saurabh Agarwal <sagarwal9698@gmail.com>
2020-07-09 13:00:18 +0530

    Added Feature: Check Winner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Container, Row, Col } from 'react-grid-system';
+import ArrowKeysReact from 'arrow-keys-react';
+import shuffle from 'shuffle-array';
+
+
+function customShuffle(collection) {
+  // TODO: Write custom logic to shuffle this collection
+  return shuffle(collection);
+}
+
 
 class App extends React.Component {
   constructor(props) {
@@ -8,11 +17,27 @@ class App extends React.Component {
     this.state = {
       value1 : 1 ,  value2 : 2 , value3 : 3,
       value4 : 4 ,  value5 : 5 , value6 : 6,
-      value7 : 7 ,  value8 : 8 , value0 : 0
+      value7 : 7 ,  value8 : 8 , value0 : 0,
+      status : "Click Start to begin the game."
     };
 
     this.handleStart = this.handleStart.bind(this);
-  }
+    this.checkWin = this.checkWin.bind(this);
+    ArrowKeysReact.config({
+      left: () => {
+        console.log('left key detected.');
+      },
+      right: () => {
+        console.log('right key detected.');
+      },
+      up: () => {
+        console.log('up key detected.');
+      },
+      down: () => {
+        console.log('down key detected.');
+      }
+    });
+  } 
 
   // randomizer(){
   //   const max = 8;
@@ -32,25 +57,48 @@ class App extends React.Component {
   //   });
   // }
 
+  checkWin() {
+
+    const checkValue0 =  this.state.value0, checkValue1 = this.state.value1, checkValue2 = this.state.value2,
+    checkValue3 = this.state.value3, checkValue4 = this.state.value4, checkValue5 = this.state.value5,
+    checkValue6 = this.state.value6, checkValue7 = this.state.value7, checkValue8 = this.state.value8;
+    
+    this.handleStart();
+
+    if(checkValue0 === 0 && checkValue1 === 1 && checkValue2 === 2 &&
+      checkValue3 === 3 && checkValue4 === 4 && checkValue5 === 5 &&
+      checkValue6 === 6 && checkValue7 === 7 && checkValue8 ===8){
+        
+      this.setState({status : "You Win!!!"});
+    }
+
+    else{
+      this.setState({status : "You Loose!!!"});
+
+    }
+  }
+
+
   handleStart() {
-
-    var shuffle = require('shuffle-array'),collection = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-
-    shuffle(collection);
+    const collection = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    customShuffle(collection);
 
     console.log(collection);
 
     this.setState({
       value1: collection[1], value2: collection[2], value3: collection[3],
       value4: collection[4], value5: collection[5], value6: collection[6],
-      value7: collection[7], value8: collection[8], value0: collection[0]
+      value7: collection[7], value8: collection[8], value0: collection[0],
+      status: ' '
     });
   }
 
 
+
+
   render() {
     return (
-      <div className="App">
+      <div className="App"  {...ArrowKeysReact.events} tabIndex="1">
         <header className="App-header">
           8 - Tiles
       </header>
@@ -94,10 +142,13 @@ class App extends React.Component {
           </Col>
           </Row>
         </Container>
+        <button className="App-switch" onClick = {this.checkWin}>
+          Submit
+        </button>
         <button className="App-switch" onClick = {this.handleStart}>
           Start
         </button>
-        <p>{this.state.valueArray}</p>
+        <p>{this.state.status}</p>
       </div>
     );
 
